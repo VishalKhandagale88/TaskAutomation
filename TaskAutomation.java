57e2ba6b3a73211e113e088823005c58b381fd2d
vishal <vishal88kh@gmail.com>
2023-08-26 01:46:06 +0530

    added exceptions

--- TaskAutomation.java
+++ TaskAutomation.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 public class TaskAutomation {
     public static void main(String[] args) {
 
@@ -6,6 +8,10 @@ public class TaskAutomation {
     public static void openChrome(String url){
         String[] command = {"cmd","/c","start",url};
         ProcessBuilder processBuilder = new ProcessBuilder(command);
-
+        try {
+            processBuilder.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
